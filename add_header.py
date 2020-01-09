8929f66542b06cb986847a2593cbe57269e940c0
Ken Youens-Clark <kyclark@gmail.com>
2020-01-09 16:51:11 -0700

    changes

--- add_header.py
+++ add_header.py
@@ -2,7 +2,7 @@
 """
 Author : Ken Youens-Clark <kyclark@gmail.com>
 Date   : 2020-01-08
-Purpose: Concat sequence files
+Purpose: Add filename to headers
 """
 
 import argparse
@@ -17,7 +17,7 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Concat sequence files, fix headers',
+        description='Add filename to headers',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('file',
@@ -27,11 +27,11 @@ def get_args():
                         help='Input file(s)')
 
     parser.add_argument('-o',
-                        '--outfile',
-                        help='Output file name',
+                        '--outdir',
+                        help='Output directory name',
                         metavar='str',
                         type=str,
-                        default='out.fa')
+                        default='with_headers')
 
     return parser.parse_args()
 
@@ -41,18 +41,25 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    out_fh = open(args.outfile, 'wt')
+    out_dir = args.outdir
+
+    if not os.path.isdir(out_dir):
+        os.makedirs(out_dir)
 
     for i, file in enumerate(args.file, start=1):
         print(f'{i:3}: {file.name}')
-        fname = re.sub('.cutforrev-t.fasta$', '', file.name)
+        base, ext = os.path.splitext(os.path.basename(file.name))
+        fname = re.sub('.cutforrev-t$', '', base)
+        out_file = os.path.join(out_dir, fname + ext)
+        out_fh = open(out_file, 'wt')
 
         for rec in SeqIO.parse(file, 'fasta'):
             new_id = '_'.join([fname, rec.id])
             out_fh.write(f'>{new_id}\n{rec.seq}\n')
 
-    out_fh.close()
-    print('Done')
+        out_fh.close()
+
+    print(f'Done, see output in "{out_dir}"')
 
 # --------------------------------------------------
 if __name__ == '__main__':
